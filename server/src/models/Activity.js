86f4d89fee7f530bc451a9377403c5fe9c82a521
Julian Andres Sosa <julian.m.sosa.muchut@gmail.com>
2023-07-05 20:19:58 -0300

    Models ok

---
+++ server/src/models/Activity.js
@@ -0,0 +1,38 @@
+const {DataTypes}=require('sequelize');
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('Activity', {
+        /* 
+        ID. *
+        Nombre. *
+        Dificultad (número del 1 al 5). *
+        Duración (en horas).
+        Temporada (Verano, Otoño, Invierno o Primavera). 
+        */
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            allowNull:false,
+            unique:true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        difficulty:{
+            type:DataTypes.ENUM(1,2,3,4,5),
+            allowNull:false,
+        },
+        duration:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+
+        },
+        season:{
+            type:DataTypes.ENUM('Summer','Autumn','Winter','Spring'),
+            allowNull:false
+        }
+
+    })
+}
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+    id:{
+      type:DataTypes.STRING(3),
+      primaryKey:true,
+      allowNull:false,
+      unique:true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    
+    flagImg:{
+      type:DataTypes.STRING,
+      validate:{
+        isUrl:true,
+      },
+      allowNull:false
+    },
+    continent:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    capital:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    subregion:{
+      type:DataTypes.STRING,
+    },
+    area:{
+      type:DataTypes.INTEGER,
+    },
+    population:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+    }
+
   });
 };
